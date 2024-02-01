a91dd9648bdc9ba358039e052e6484ced3c214d5
evanesscence <102433438+evanesscence@users.noreply.github.com>
2024-02-01 14:36:24 +0300

    loaded images

---
+++ HackingWithSwift/DetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailViewController.swift
+//  HackingWithSwift
+//
+//  Created by Karina ❦ on 01.02.2024.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let imageToLoad = selectedImage {
+//            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+}
