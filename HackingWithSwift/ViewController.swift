8060a40e8b74e3e2685d42a9b8647ab39ae9042c
Karina <barakhaeva@inbox.ru>
2024-02-08 19:30:49 +0300

    .

--- HackingWithSwift/ViewController.swift
+++ HackingWithSwift/ViewController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TableViewController: UITableViewController {
+    
+    var countries = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loadImages()
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countries.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "flag", for: indexPath)
+        cell.textLabel?.text = countries[indexPath.row]
+        return cell
+    }
+    
+    private func loadImages() {
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasSuffix("png") {
+                countries.append(item)
+            }
+        }
+    }
 }
 
