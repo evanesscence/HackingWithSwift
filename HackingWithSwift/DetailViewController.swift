90317d12f79f675a1ffcddd112d23213c6e5746b
Karina <barakhaeva@inbox.ru>
2024-02-09 17:55:54 +0300

    done

--- HackingWithSwift/DetailViewController.swift
+++ HackingWithSwift/DetailViewController.swift
@@ -9,12 +9,45 @@ import UIKit
 
 class DetailViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
-    var selectedImage: String?
+    @IBOutlet weak var quoteButton: UIButton!
     
+    var selectedImage: String?
+    var quote: String?
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        navigationItem.largeTitleDisplayMode = .never
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shared))
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+            title = selectedImage
+        }
     }
     
-
+    @objc func shared() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            return
+        }
+        
+        guard let message = quote else {
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [image, message], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
+    
+    @IBAction func showQuote(_ sender: Any) {
+        guard let message = quote else {
+            return
+        }
+        
+        let alert = UIAlertController(title: "Цитата", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Закрыть", style: .default))
+        
+        present(alert, animated: true)
+    }
 }
--- HackingWithSwift/ViewController.swift
+++ HackingWithSwift/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UITableViewController {
     
-    let books = ["Гордость и предубеждение", "Джейн Эйр", "Цветы для Элджернона", "Заводной апельсин", "Мартин Иден"]
+    struct Book {
+        let title: String
+        let image: String
+        let quote: String
+    }
+    
+    var books = [
+        Book(title: "Гордость и предубеждение", image: "Гордость и предубеждение", quote: "Ещё никогда она не осознавала с такой отчётливостью, насколько сильно она могла бы его полюбить, как именно сейчас, в ту самую минуту, когда ни о какой любви между ними больше не могло быть и речи."),
+        
+        Book(title: "Джейн Эйр", image: "Джейн Эйр", quote: "Когда нас бьют без причины, мы должны отвечать ударом на удар, и притом с такой силой, чтобы навсегда отучить людей бить нас."),
+        
+        Book(title: "Цветы для Элджернона", image: "Цветы для Элджернона", quote: "Раньше меня презирали за невежество и тупость, теперь ненавидят за ум и знания. Господи, да чего же им нужно от меня?"),
+        
+        Book(title: "Заводной апельсин", image: "Заводной апельсин", quote: "Аминь. И всякий прочий kal."),
+        
+        Book(title: "Мартин Иден", image: "Мартин Иден", quote: "Ограниченные умы видят ограниченность только в других.")
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        books.sort(by: {$0.title < $1.title})
         
+        title = "Книги"
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -22,14 +41,23 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath)
-        cell.textLabel?.text = books[indexPath.row]
-        cell.imageView?.image = UIImage(named: books[indexPath.row])
+        
+        cell.textLabel?.text = books[indexPath.row].title
+        cell.textLabel?.font = UIFont(name: "system", size: 22)
         
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        <#code#>
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = books[indexPath.row].image
+            vc.quote = books[indexPath.row].quote
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 56
     }
     
 }
