d2663c0bf0a493e7111fbc64b83434b13ffc2643
Karina <barakhaeva@inbox.ru>
2024-02-09 15:57:48 +0300

    in process

---
+++ HackingWithSwift/DetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailViewController.swift
+//  HackingWithSwift
+//
+//  Created by Karina ❦ on 09.02.2024.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
--- HackingWithSwift/ViewController.swift
+++ HackingWithSwift/ViewController.swift
@@ -9,9 +9,7 @@ import UIKit
 
 class ViewController: UITableViewController {
     
-    let books = [String]()
-    
-    
+    let books = ["Гордость и предубеждение", "Джейн Эйр", "Цветы для Элджернона", "Заводной апельсин", "Мартин Иден"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,8 +17,20 @@ class ViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return books.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath)
+        cell.textLabel?.text = books[indexPath.row]
+        cell.imageView?.image = UIImage(named: books[indexPath.row])
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         <#code#>
     }
-
+    
 }
 
