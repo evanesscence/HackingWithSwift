6e84ef472a8df22fc6198431d0b61b1256ea7025
Karina <barakhaeva@inbox.ru>
2024-02-14 17:54:52 +0300

    .

--- HackingWithSwift/TableViewController.swift
+++ HackingWithSwift/TableViewController.swift
@@ -14,6 +14,12 @@ class TableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = websites[indexPath.row]
+        
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+      
     }
 }
